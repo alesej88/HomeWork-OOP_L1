225f4ebdb5cc868da4c180adbbf4dcbcf73254f2
aleksey <alesej88@ya.ru>
2021-03-26 22:46:23 +0600

    ex#1

---
+++ src/main/java/OOP_L1.java
@@ -0,0 +1,24 @@
+public class OOP_L1 {
+    static class Employee {
+        int age;
+        String name;
+        String surname;
+        long tel;
+        String email;
+        double salary;
+
+
+        public static void main(String[] args) {
+            Employee emp = new Employee();
+            emp.age = 30;
+            emp.name = "Ivan";
+            emp.surname = "Petrov";
+            emp.tel = 89003332220l;
+            emp.email = "ip@gmail.com";
+            emp.salary = 30000;
+            System.out.println("Возраст сотрудника: " + emp.age + " лет" + " " + "Имя: " + emp.name + " " + "Фамилия: " + emp.surname + " " + "телефон: " + emp.tel + " " + "емайл: " + emp.email + " " + "зарплата: " + emp.salary);
+
+
+        }
+    }
+}
